efa372262a49a254b9b6f4aaa534a430f4c7a753
Sukhpreet Singh <panesar.pb08@gmail.com>
2024-12-09 13:04:00 -0500

    init

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,7 +3,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const routes: ServerRoute[] = [
   {
     path: 'login',
-    renderMode: RenderMode.Server,
+    renderMode: RenderMode.Client,
   },
   {
     path: '**',
